0b17d7d656270880f963fdbae3e92f2fc06cf173
DanLevyM <dan.levy.pro@gmail.com>
2021-12-16 15:27:32 +0100

    archi

--- blackJack/index.js
+++ blackJack/index.js
@@ -1,5 +1,5 @@
-import Controller from './Controller/controller.js';
-import Model from './Model/model.js';
-import View from './View/view.js';
+import Controller from './script/controller.js';
+import Model from './script/model.js';
+import View from './script/view.js';
 
 const app = new Controller(new Model(), new View());
